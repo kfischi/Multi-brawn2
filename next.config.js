4a1db177d7728def60ece3d10a61fc69675cd6f1
kfischi <kfir.biz@gmail.com>
2025-11-18 22:28:40 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,7 +8,11 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'images.unsplash.com',
+        hostname: 'github.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
       },
     ],
   },
